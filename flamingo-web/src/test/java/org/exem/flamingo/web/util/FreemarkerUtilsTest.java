024d31b769cc2e850afbbb045bdbf7a5e7654aba
김병곤 <fharenheit@ex-em.com>
2016-10-12 15:40:18 +0900

    FL-96 Freemarker로 변경

--- flamingo-web/src/test/java/org/exem/flamingo/web/util/FreemarkerUtilsTest.java
+++ flamingo-web/src/test/java/org/exem/flamingo/web/util/FreemarkerUtilsTest.java
@@ -4,13 +4,23 @@ import freemarker.template.Configuration;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class FreemarkerUtilsTest {
 
     @Test
     public void evaluate() throws Exception {
-        ApplicationContext ctx = new ClassPathXmlApplicationContext("org.exem.flamingo.web.oozie/applicationContext.xml");
-        Configuration conf = ctx.getBean(Configuration.class);
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("org/exem/flamingo/web/oozie/applicationContext.xml");
+        FreeMarkerConfigurer configurer = ctx.getBean(FreeMarkerConfigurer.class);
+        Configuration conf = configurer.getConfiguration();
+
+        Map params = new HashMap();
+        params.put("name", "Freemarker");
+
+        FreemarkerUtils.evaluate(conf, "org/exem/flamingo/web/oozie/helloworld.ftl", params);
     }
 
 }
