909ea4d2292f542fb91e6bbd98deeb9e67e2aa2f
김병곤 <fharenheit@ex-em.com>
2016-10-16 08:39:32 +0900

    라이센스 문구 추가

--- flamingo-web/src/main/java/org/exem/flamingo/web/logging/WebLogbackConfigurer.java
+++ flamingo-web/src/main/java/org/exem/flamingo/web/logging/WebLogbackConfigurer.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2012-2016 the Flamingo Community.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.exem.flamingo.web.logging;
 
 import ch.qos.logback.core.joran.spi.JoranException;
@@ -17,8 +33,6 @@ import java.lang.reflect.Method;
  * allowing for log file paths within the web application.
  *
  * @author Byoung Gon, Kim
- * @see org.springframework.util.Log4jConfigurer
- * @see org.springframework.web.util.Log4jConfigListener
  * @since 1.0
  */
 public class WebLogbackConfigurer {
