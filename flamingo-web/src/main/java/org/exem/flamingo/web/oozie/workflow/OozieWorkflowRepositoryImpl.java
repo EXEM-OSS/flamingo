ff655fe193e9ac4e17549650c2ff49d31f571abf
sanghyunbak <sanghyunbak@gmail.com>
2016-12-02 18:00:55 +0900

    FL-102 PersistentRepositoryImpl에 있는 기존 CRUD를 override 해서 사용

--- flamingo-web/src/main/java/org/exem/flamingo/web/oozie/workflow/OozieWorkflowRepositoryImpl.java
+++ flamingo-web/src/main/java/org/exem/flamingo/web/oozie/workflow/OozieWorkflowRepositoryImpl.java
@@ -25,33 +25,28 @@ public class OozieWorkflowRepositoryImpl extends PersistentRepositoryImpl implem
     super.setSqlSessionTemplate(flamingoSessionTemplate);
   }
 
-  @Override
   public Map selectTreeId(String jobId) {
     return this.getSqlSessionTemplate().selectOne(this.getNamespace() + ".selectJobId", jobId);
   }
 
-  @Override
   public List<Map> listWorkflows() {
     return this.getSqlSessionTemplate().selectList(this.getNamespace() + ".listWorkflows");
   }
 
-  @Override
   public Map getRecentWorkflow() {
     return this.getSqlSessionTemplate().selectOne(this.getNamespace() + ".selectRecentWorkflow");
   }
 
-  @Override
-  public void insertWorkflow(Map param) {
-    this.getSqlSessionTemplate().insert(this.getNamespace() + ".insertWorkflow", param);
-  }
-
-  @Override
   public void updateWorkflow(Map param) {
     this.getSqlSessionTemplate().insert(this.getNamespace() + ".updateWorkflow", param);
   }
 
-  @Override
   public void deleteWorkflow(long id) {
     this.getSqlSessionTemplate().delete(this.getNamespace() + ".deleteWorkflow", id);
+  }
+
+  @Override
+  public void insert(Map param){
+    this.getSqlSessionTemplate().insert(this.getNamespace() + ".insert", param);
   }
 }
