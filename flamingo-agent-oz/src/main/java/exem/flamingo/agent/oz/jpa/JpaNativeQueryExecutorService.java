773642baeca8b934f2d5d59e463ea5bf3dbfb41f
김경식 <ersione@ex-em.com>
2016-12-07 16:29:33 +0900

    FL-108
    펑션 체크 추가

--- flamingo-agent-oz/src/main/java/exem/flamingo/agent/oz/jpa/JpaNativeQueryExecutorService.java
+++ flamingo-agent-oz/src/main/java/exem/flamingo/agent/oz/jpa/JpaNativeQueryExecutorService.java
@@ -7,6 +7,8 @@ import org.apache.oozie.service.ServiceException;
 import org.apache.oozie.service.Services;
 import org.apache.oozie.util.XLog;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import java.io.IOException;
 import java.util.Properties;
 
@@ -29,6 +31,11 @@ public class JpaNativeQueryExecutorService implements Service {
 
         loadConfig();
         LOG.info("[Flamingo] Finish init config.");
+
+        if (!checkFunction()) {
+            makeFunction();
+        }
+
         LOG.info("[Flamingo] Finish.");
     }
 
@@ -49,6 +56,63 @@ public class JpaNativeQueryExecutorService implements Service {
         } catch (IOException e) {
             config.setProperty("port", "31000");
             e.printStackTrace();
+        }
+    }
+
+    private boolean checkFunction() {
+        boolean result = false;
+
+        EntityManager entityManager = jpaService.getEntityManager();
+        try {
+            entityManager.getTransaction().begin();
+            Query query = entityManager.createNativeQuery("SELECT DATE_TRUNC('hour', CURRENT_DATE)");
+            Object queryResult = query.getSingleResult();
+            result = true;
+        } catch (Exception e) {
+
+        } finally {
+            entityManager.getTransaction().rollback();
+            if (entityManager.isOpen()) {
+                entityManager.close();
+            }
+        }
+
+        return result;
+    }
+
+    private void makeFunction() {
+        EntityManager entityManager = jpaService.getEntityManager();
+        try {
+
+            String makeFunctionQuery = "DELIMITER //\n" +
+                    "create function date_trunc(vInterval varchar(7), vDate timestamp)\n" +
+                    "  returns timestamp\n" +
+                    "  begin\n" +
+                    "    declare toReturn timestamp;\n" +
+                    "\n" +
+                    "    if vInterval = 'year' then set toReturn = date_add('1900-01-01', interval TIMESTAMPDIFF(YEAR, '1900-01-01', vDate) YEAR);\n" +
+                    "    elseif vInterval = 'quarter' then set toReturn = date_add('1900-01-01', interval TIMESTAMPDIFF(QUARTER, '1900-01-01', vDate) QUARTER);\n" +
+                    "    elseif vInterval = 'month' then set toReturn = date_add('1900-01-01', interval TIMESTAMPDIFF(MONTH, '1900-01-01', vDate) MONTH);\n" +
+                    "    elseif vInterval = 'week' then set toReturn = date_add('1900-01-01', interval TIMESTAMPDIFF(WEEK, '1900-01-01', vDate) WEEK);\n" +
+                    "    elseif vInterval = 'day' then set toReturn = date_add('1900-01-01', interval TIMESTAMPDIFF(DAY, '1900-01-01', vDate) DAY);\n" +
+                    "    elseif vInterval = 'hour' then set toReturn = date_add('1900-01-01', interval TIMESTAMPDIFF(HOUR, '1900-01-01', vDate) HOUR);\n" +
+                    "    elseif vInterval = 'minute' then set toReturn = date_add('1900-01-01', interval TIMESTAMPDIFF(MINUTE, '1900-01-01', vDate) MINUTE);\n" +
+                    "    END IF;\n" +
+                    "\n" +
+                    "    return toReturn;\n" +
+                    "  end//\n" +
+                    "DELIMITER ;";
+
+            entityManager.getTransaction().begin();
+            Query query = entityManager.createNativeQuery(makeFunctionQuery);
+            query.executeUpdate();
+        } catch (Exception e) {
+
+        } finally {
+            entityManager.getTransaction().rollback();
+            if (entityManager.isOpen()) {
+                entityManager.close();
+            }
         }
     }
 }
