ab4f6721c2d1aa592de71ce340f0d5f2d5d5e6a9
김경식 <ersione@ex-em.com>
2016-12-07 16:27:29 +0900

    FL-108
    기초 셋팅

--- flamingo-agent-oz/src/main/java/exem/flamingo/agent/oz/jpa/JpaNativeQueryExecutorService.java
+++ flamingo-agent-oz/src/main/java/exem/flamingo/agent/oz/jpa/JpaNativeQueryExecutorService.java
@@ -1,5 +1,36 @@
 package exem.flamingo.agent.oz.jpa;
 
-public class JpaNativeQueryExecutorService {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.oozie.service.JPAService;
+import org.apache.oozie.service.Service;
+import org.apache.oozie.service.ServiceException;
+import org.apache.oozie.service.Services;
+import org.apache.oozie.util.XLog;
 
+public class JpaNativeQueryExecutorService implements Service {
+
+    private static XLog LOG;
+
+    private JPAService jpaService;
+
+    private ObjectMapper objectMapper;
+
+
+    @Override
+    public void init(Services services) throws ServiceException {
+        LOG = XLog.getLog(JpaNativeQueryExecutorService.class);
+        LOG.info("[Flamingo] Start init.");
+        objectMapper = new ObjectMapper();
+        jpaService = services.get(JPAService.class);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+    @Override
+    public Class<? extends Service> getInterface() {
+        return null;
+    }
 }
